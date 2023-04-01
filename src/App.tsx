5c3f62677791c6c31b0b2b94fd13c6d6b99921cf
alex-sandler <troubadour1488@gmail.com>
2023-04-02 01:45:04 +0300

    Работа с компонентами через пропсы

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
+import Rating from "./components/Rating/Rating";
 
 function App() {
     return (
         <div className="App">
-            {/*<div>
+            <div>
+                {/*<div>
                 <h3>What to learn</h3>
                 <div>
                     <input/>
@@ -22,27 +24,20 @@ function App() {
                     <button>Completed</button>
                 </div>
             </div>*/}
-            <Accordion/>
+                <PageTitle title={"This is APP component"}/>
+                Article 1
+                <Rating value={3}/>
+                <Accordion value={"Меню"}/>
+                Article 2
+                <Rating value={5}/>
+            </div>
         </div>
     );
 }
 
-function Rating() {
-    console.log("Rating rendering")
-    return (
-        <>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </>
-    )
-}
-
-function Star() {
-    console.log("Star rendering")
-    return <div>star</div>
+function PageTitle(props: any) {
+    console.log("PageTitle rendering")
+    return <h1>{props.title}</h1>
 }
 
 export default App;
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 
-function Accordion() {
+function Accordion(props: any) {
     return <div>
-        <AccordionTitle/>
+        <AccordionTitle title={props.value}/>
         <AccordionBody/>
     </div>
 }
 
-function AccordionTitle() {
+function AccordionTitle(props: any) {
     console.log("AccordionTitle rendering")
-    return (
-        <h3>Меню</h3>
-    )
+    return <h3>{props.title}</h3>
+
 }
 
 function AccordionBody() {
